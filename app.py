2bf7140408c0c32b5169f138c6137f8c09446112
19229 <19229@wc.school.nz>
2023-03-24 13:47:29 +1300

    starting to use vcs, connecting database

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template
+import sqlite3
+
+
+DATABASE = "coffee.db"
+
+app = Flask(__name__)
+
+
+def create_connection(db_file):
+    try:
+        connection = sqlite3.connect(db_file)
+        return connection
+    except ValueError as e:
+        print(e)
+    return None
+
+
+
+@app.route('/')
+def render_home():
+    return render_template('home.html')
+
+
+@app.route('/menu')
+def render_menu():
+    con = create_connection(DATABASE)
+    query = "SELECT name, description, volume, image, price FROM products"
+    cur = con.cursor()
+    cur.execute(query)
+    product_list = cur.,fe
+
+    return render_template('menu.html')
+
+
+@app.route('/contact')
+def render_contact():
+    return render_template('contact.html')
+
+
+app.run(host='0.0.0.0', debug=True)
