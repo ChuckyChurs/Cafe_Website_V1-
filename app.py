f288f03ba4693afb2dc3289faa6d9858508a474a
19229 <19229@wc.school.nz>
2023-04-05 10:16:11 +1200

    singup

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
 import sqlite3
+from sqlite3 import Error
+from flask_bcrypt import Bcrypt
 
 
-DATABASE = "coffee.db.db"
+DATABASE = "C:/Users/19229/OneDrive - Wellington College/13 DTS/coffee/coffee.db.db"
 
 app = Flask(__name__)
+bcrypt = Bcrypt(app)
+app.secret_key = "kzgsdlit3e"
 
 
-def create_connection(db_file):
+def open_database(db_file):
     try:
         connection = sqlite3.connect(db_file)
         return connection
@@ -24,7 +28,7 @@ def render_home():
 
 @app.route('/menu/<cat_id>')
 def render_menu(cat_id):
-    con = create_connection(DATABASE)
+    con = open_database(DATABASE)
     query = "SELECT name, description, volume, image, price FROM products WHERE cat_id=?"
     cur = con.cursor()
     cur.execute(query, (cat_id, ))
@@ -41,6 +45,42 @@ def render_menu(cat_id):
 @app.route('/contact')
 def render_contact():
     return render_template('contact.html')
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def render_login():
+    return render_template('login.html')
+
+@app.route('/signup', methods=['POST', 'GET'])
+def render_signup():
+    if request.method == "POST":
+        print(request.form)
+        fname = request.form.get('fname').title().strip()
+        lname = request.form.get('lname').title()
+        email = request.form.get('email').lower().strip()
+        password = request.form.get('password')
+        password2 = request.form.get('password2')
+
+        if password != password2:
+            return redirect("\signup?error=Passwords+do+not+match")
+        if len(password) < 8:
+            return redirect("\signup?error=Password+msut+be+at+least+8+characters+long")
+
+        hashed_password = bcrypt.generate_password_hash(password)
+        con = open_database(DATABASE)
+        query = "INSERT INTO user(fname, lname, email, password) VALUES (?, ?, ?, ?)"
+        cur = con.cursor()
+
+        try:
+            cur.execute(query, (fname, lname, email, hashed_password))
+        except:
+            con.close()
+            return redirect("\signup?error=Email+is+already+used")
+
+        con.commit()
+        con.close()
+
+    return render_template('signup.html')
 
 
 app.run(host='0.0.0.0', debug=True)
